6adf18d021ed5c957894d4484c4615a356679f82
Pradnya <bhujbalpradnya@gmail.com>
2023-02-14 15:53:53 -0500

    Update for custom hook

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,12 +1,20 @@
-import {useParams} from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import useProductDetails from "../utils/useProductList";
 
 const ProductDetails =()=>{
 
-    const params = useParams();
-    console.log("params", params.id)
+     const params = useParams();
+     //console.log("params", params.prodId)
+
+     const {productDetails}= useProductDetails(params.prodId)
     return(
-        <div>
-            
+        <div className="border text-center p-2 ">
+           <div>{productDetails.title}</div>  
+           <img className="w-10 m-auto" src={productDetails.image} alt='Product'/>
+           <div>{productDetails.description}</div>
+           <div>Price - {productDetails.price}</div>
+           {/* <div>{productDetails.rating.rate} star</div> */}
+           <button className="bg-green-500 text-white p-2"> Add to Cart</button>
         </div>
     )
 
--- src/utils/useProductList.js
+++ src/utils/useProductList.js
@@ -1,21 +1,20 @@
 import {useState, useEffect} from 'react';
 
-const useProductDetails =()=>{
-
-    
-   
+const useProductDetails =(productId)=>{
+    const [productDetails , setProductDetails]= useState({});  
 
     useEffect(()=>{
       getProductDetails();
     },[]);
 
     async function getProductDetails(){
-       
-        
+       const response = await fetch(`https://fakestoreapi.com/products/${productId}`)
+       const data =  await response.json();   
+       setProductDetails(data);
     }
 
    return {
-       
+       productDetails
    }
 }
 
